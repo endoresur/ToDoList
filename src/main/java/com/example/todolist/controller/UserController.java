f031e5e6f9896fc69e18e42b489b833fb2d95a6b
endoresur <mlhv2lv@gmail.com>
2021-10-22 21:05:34 +0500

    Add methods to delete and get one user

--- src/main/java/com/example/todolist/controller/UserController.java
+++ src/main/java/com/example/todolist/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.todolist.controller;
 
 import com.example.todolist.entity.UserEntity;
 import com.example.todolist.exceptions.UserAlreadyExistException;
+import com.example.todolist.exceptions.UserNotFoundException;
 import com.example.todolist.repository.UserRepository;
 import com.example.todolist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,14 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/all")
-    public ResponseEntity getUsers() {
+    @GetMapping
+    public ResponseEntity getOneUser(@RequestParam Long id) {
         try {
-            return ResponseEntity.ok("Сервер работает");
-        } catch (Exception e) {
+            return ResponseEntity.ok(userService.getOne(id));
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        catch (Exception e) {
             return ResponseEntity.badRequest().body("Ошибка");
         }
     }
@@ -33,6 +37,15 @@ public class UserController {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Ошибка");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(userService.delete(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
         }
     }
 }
--- src/main/java/com/example/todolist/service/UserService.java
+++ src/main/java/com/example/todolist/service/UserService.java
@@ -2,6 +2,8 @@ package com.example.todolist.service;
 
 import com.example.todolist.entity.UserEntity;
 import com.example.todolist.exceptions.UserAlreadyExistException;
+import com.example.todolist.exceptions.UserNotFoundException;
+import com.example.todolist.model.User;
 import com.example.todolist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,5 +20,18 @@ public class UserService {
             throw new UserAlreadyExistException("Пользователь с таким именем уже существует");
         }
         return userRepository.save(user);
+    }
+
+    public User getOne(Long id) throws UserNotFoundException {
+        var user = userRepository.findById(id).get();
+        if (user == null) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
+        return User.toModel(user);
+    }
+
+    public Long delete(Long id) {
+        userRepository.deleteById(id);
+        return id;
     }
 }
