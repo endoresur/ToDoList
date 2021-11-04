08cb05300beaf8227f7ce18027cae4ee48881fd7
endoresur <mlhv2lv@gmail.com>
2021-11-04 23:57:11 +0500

    Remove comments from code

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -4,7 +4,6 @@ class UserList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {todos: []};
-        //this.remove = this.remove.bind(this);
     }
 
     async componentDidMount() {
