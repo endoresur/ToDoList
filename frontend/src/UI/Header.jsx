f1e5770294ed7c6ab2a9b5dcadef42344b565532
endoresur <mlhv2lv@gmail.com>
2021-11-08 16:57:19 +0500

    Rename MyNavbar to Header and remade design

--- frontend/src/UI/Header.jsx
+++ frontend/src/UI/Header.jsx
@@ -2,22 +2,23 @@ import logo from './../logo.svg';
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Nav, Button} from "reactstrap";
-import {Link} from "react-router-dom";
 
-class MyNavbar extends React.Component {
+class Header extends React.Component {
 
     render() {
         return (
-            <Navbar className={"row p-0"}>
+            <Navbar className={"p-0"}>
                 <Nav className={"bg-dark w-100"}>
-                    <div className="container-fluid d-inline-flex w-100">
+                    <div className="container">
                         <img src={logo} alt="logo" height={70} width={70}/>
-                        <Button className={"bg-black m-2"}>Registration</Button>
+                        1234
+                        Qwerty
                     </div>
+                    <Button className={"bg-black m-2"}>Registration</Button>
                 </Nav>
             </Navbar>
         )
     }
 }
 
-export default MyNavbar;
+export default Header;
