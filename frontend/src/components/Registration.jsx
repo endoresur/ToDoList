b02aa198d4f6c77dfef295a8995b7ed2d8dc860c
endoresur <mlhv2lv@gmail.com>
2021-11-15 00:13:00 +0500

    Create post request to registration

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -1,23 +1,42 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form, Input, Container, Button} from "reactstrap";
 
-class Registration extends React.Component {
+function Registration() {
 
-    constructor(props) {
-        super(props);
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const addUser = () => {
+        fetch('/users/reg', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+        })
+            .then((res) => res.json())
+            .catch((err) => console.log('error'))
     }
 
-    componentDidMount() {
-        const response = fetch('/users/reg');
-    }
-
-    render() {
-        return (
-            <div>
-                Registration page
-            </div>
-        );
-    }
+    return (
+        <Form className="w-25">
+            <Container>
+                <label htmlFor='username'>Username</label>
+                <Input type='username' id='username' value={username}
+                       onChange={(e) => setUsername(e.target.value)} />
+            </Container>
+            <Container>
+                <label htmlFor='password'>Password</label>
+                <Input type='password' id='password' value={password}
+                       onChange={(e) => setPassword(e.target.value)} />
+            </Container>
+            <Button onClick={(e) => addUser()}>Log in</Button>
+        </Form>
+    );
 }
 
 export default Registration;
