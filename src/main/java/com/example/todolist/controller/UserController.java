76e5a8cccf6e4b6bfc96566aebc8530c1bd9eb17
endoresur <mlhv2lv@gmail.com>
2021-11-18 23:34:52 +0500

    Remove registration from user controller

--- src/main/java/com/example/todolist/controller/UserController.java
+++ src/main/java/com/example/todolist/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.todolist.controller;
 
 import com.example.todolist.entity.UserEntity;
 import com.example.todolist.exceptions.UserAlreadyExistException;
-import com.example.todolist.exceptions.UserNotFoundException;
 import com.example.todolist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +17,7 @@ public class UserController {
     @GetMapping("/all")
     public ResponseEntity getUsers(){
         try {
-            return ResponseEntity.ok(userService.getUsers());
+            return ResponseEntity.ok(userService.allUsers());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Ошибка");
         }
@@ -27,22 +26,7 @@ public class UserController {
     @GetMapping
     public ResponseEntity getOneUser(@RequestParam Long id) {
         try {
-            return ResponseEntity.ok(userService.getOne(id));
-        } catch (UserNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-        catch (Exception e) {
-            return ResponseEntity.badRequest().body("Ошибка");
-        }
-    }
-
-    @PostMapping("/reg")
-    public ResponseEntity registration(@RequestBody UserEntity user) {
-        try {
-            userService.registration(user);
-            return ResponseEntity.ok("Пользователь добавлен");
-        } catch (UserAlreadyExistException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.ok(userService.findUserById(id));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Ошибка");
         }
@@ -51,7 +35,7 @@ public class UserController {
     @DeleteMapping("/{id}")
     public ResponseEntity deleteUser(@PathVariable Long id) {
         try {
-            return ResponseEntity.ok(userService.delete(id));
+            return ResponseEntity.ok(userService.deleteUser(id));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Произошла ошибка");
         }
