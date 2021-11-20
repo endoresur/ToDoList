f62a7c21962c4dcd5f7a790507c3859f6482bc7f
endoresur <mlhv2lv@gmail.com>
2021-11-20 16:48:49 +0500

    Use try catch block to registration get mapping

--- src/main/java/com/example/todolist/controller/RegistrationController.java
+++ src/main/java/com/example/todolist/controller/RegistrationController.java
@@ -1,15 +1,11 @@
 package com.example.todolist.controller;
 
 import com.example.todolist.entity.UserEntity;
-import com.example.todolist.model.User;
 import com.example.todolist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,8 +17,12 @@ public class RegistrationController {
 
     @GetMapping("/registration")
     public ResponseEntity registration() {
-        UserEntity user = new UserEntity();
-        return ResponseEntity.ok(user);
+        try {
+            UserEntity user = new UserEntity();
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Ошибка");
+        }
     }
 
     @PostMapping("/registration")
