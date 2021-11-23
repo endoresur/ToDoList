3055d6ea356499a43e030d64a1cbb11953cf2386
endoresur <mlhv2lv@gmail.com>
2021-11-24 00:11:45 +0500

    Redesign header

--- frontend/src/UI/Header.jsx
+++ frontend/src/UI/Header.jsx
@@ -1,32 +1,44 @@
-import logo from './../logo.svg';
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav, Button} from "reactstrap";
+import React from 'react';
+import logo from "../logo.svg";
 import {Link} from "react-router-dom";
+import {Input} from "reactstrap";
 
-class Header extends React.Component {
+const Header = () => {
+    return (
+        <div>
+            <header className="p-3 bg-dark text-white">
+                <div className="container">
+                    <div
+                        className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                            <img src={logo} alt="logo" height={32} width={40}/>
+                        </a>
 
-    render() {
-        return (
-            <Navbar className={"p-0 w-100"}>
-                <Nav className={"bg-dark w-100"}>
-                    <div className={"container w-100 d-flex"}>
-                        <div className="w-100 me-auto">
-                            <img src={logo} alt="logo" height={70} width={70}/>
-                            <Link to={"/main"}>Home</Link>
-                            <Link to={"/todos"}>Tasks</Link>
-                            <Link to={"/about"}>About</Link>
-                        </div>
-                        <div className={"ms-auto p-1 w-25 flex-shrink-1"}>
+                        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                            <li><Link to={"/main"} className="nav-link px-2 text-white">Home</Link></li>
+                            <li><Link to={"/todos"} className="nav-link px-2 text-white">Tasks</Link></li>
+                            <li><Link to={"/about"} className="nav-link px-2 text-white">Features</Link></li>
+                            <li><Link to={"/about"} className="nav-link px-2 text-white">About</Link></li>
+                        </ul>
+
+                        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                            <Input type="search" className="form-control form-control-dark" placeholder="Search..."
+                                   aria-label="Search"/>
+                        </form>
+
+                        <div className="text-end">
                             <Link to={"/registration"}>
-                                <Button className={"btn btn-primary p-2 m-2"}>Sign in</Button>
+                                <button type="button" className="btn btn-outline-light me-2">Login</button>
+                            </Link>
+                            <Link to={"/registration"}>
+                                <button type="button" className="btn btn-primary">Sign-up</button>
                             </Link>
                         </div>
                     </div>
-                </Nav>
-            </Navbar>
-        )
-    }
-}
+                </div>
+            </header>
+        </div>
+    );
+};
 
 export default Header;
