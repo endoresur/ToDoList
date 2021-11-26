051a0a5f872ab97aff696057cd96ed09ae402547
endoresur <mlhv2lv@gmail.com>
2021-11-27 00:17:38 +0500

    Create UI component to add new tasks and add routing to it

---
+++ frontend/src/components/TodoCreate.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Container} from "reactstrap";
+
+class TodoCreate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return(
+            <div>
+                123
+            </div>
+        )
+    }
+
+}
+
+export default TodoCreate;
