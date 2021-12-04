e8b073570610169d8bbbd2adc833475afc5950a4
endoresur <mlhv2lv@gmail.com>
2021-12-04 20:13:15 +0500

    Add UI improvements

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -21,38 +21,37 @@ class TodoList extends React.Component {
         const {todos} = this.state;
         return (
             <Container className={"m-4 p-4"}>
+                <div className="container">
 
-                    <div className="container">
+                    <Container className="row g-4 py-5 row-cols-1 row-cols-lg-3 pb-2 border-bottom">
+                        <div className="feature col">
+                            <h2 className="">Active tasks</h2>
+                        </div>
+                        <div className="feature col"/>
+                        <div className="feature col">
+                            <Link to="/todo-create">
+                                <button type="button" className="w-100 btn btn-lg btn-outline-primary">
+                                    Create task
+                                </button>
+                            </Link>
+                        </div>
+                    </Container>
 
-                        <Container className="row g-4 py-5 row-cols-1 row-cols-lg-3 pb-2 border-bottom">
-                            <div className="feature col">
-                                <h2 className="">Active tasks</h2>
-                            </div>
-                            <div className="feature col"/>
-                            <div className="feature col">
-                                <Link to="/todo-create">
-                                    <button type="button" className="w-100 btn btn-lg btn-outline-primary">
-                                        Create task
-                                    </button>
-                                </Link>
-                            </div>
-                        </Container>
-
-                        {todos.reverse().map(todo =>
-                            <Card
-                                key={todo.id}
-                                style={{margin: '20px'}}>
-                                <Card.Header>
-                                    <h3>{todo.title}</h3>
-                                    Author: {todo.username}
-                                </Card.Header>
-                                <Card.Body>
-                                    <Card.Text>{todo.description}</Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        )}
-                    </div>
+                    {todos.reverse().map(todo =>
+                        <Card
+                            key={todo.id}
+                            style={{margin: '20px'}}>
+                            <Card.Header>
+                                <h3>{todo.title}</h3>
+                                Author: {todo.username}
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text>{todo.description}</Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    )}
+                </div>
             </Container>
         )
     }
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -21,6 +21,36 @@ const Main = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="container px-4 py-5" id="featured-3">
+                <h2 className="pb-2 border-bottom">Columns with icons</h2>
+                <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                    <div className="feature col">
+                        <h2>Featured title</h2>
+                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                            sentence and probably just keep going until we run out of words.</p>
+                        <a href="#" className="icon-link">
+                            Call to action
+                        </a>
+                    </div>
+                    <div className="feature col">
+                        <h2>Featured title</h2>
+                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                            sentence and probably just keep going until we run out of words.</p>
+                        <a href="#" className="icon-link">
+                            Call to action
+                        </a>
+                    </div>
+                    <div className="feature col">
+                        <h2>Featured title</h2>
+                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                            sentence and probably just keep going until we run out of words.</p>
+                        <a href="#" className="icon-link">
+                            Call to action
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
